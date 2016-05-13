290fc0dbbc220fc9fda1998d35095bf352999a5a
Yuval Drori <yuval.drori@gmail.com>
2016-05-13 22:45:33 +0300

    check perms every time

--- app/src/main/java/me/drori/forty/FortyNotificationListenerService.java
+++ app/src/main/java/me/drori/forty/FortyNotificationListenerService.java
@@ -1,9 +1,12 @@
 package me.drori.forty;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -13,7 +16,6 @@ public class FortyNotificationListenerService extends NotificationListenerServic
     public static final String TAG = "FORTY";
     private static final String CALENDAR_NAME = "Forty";
 
-    private Calendar calendar;
     private static Context context;
 
     public static Context getContext() {
@@ -24,7 +26,12 @@ public class FortyNotificationListenerService extends NotificationListenerServic
     public void onCreate() {
         super.onCreate();
         context = this;
-        this.calendar = new Calendar(CALENDAR_NAME);
+    }
+
+    private boolean hasPermissions() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED)
+                && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) == PackageManager.PERMISSION_GRANTED)
+                && (ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED);
     }
 
     @Override
@@ -38,6 +45,10 @@ public class FortyNotificationListenerService extends NotificationListenerServic
     }
 
     private void act(StatusBarNotification sbn) {
+        if (!hasPermissions()) {
+            return;
+        }
+        Calendar calendar = new Calendar(CALENDAR_NAME);
         String pkgName = sbn.getPackageName();
 
         if (BuildConfig.DEBUG) {
